0cdc7493d72b463bd619266554ec2a5c58389923
SuperThunder <secretagentsora@gmail.com>
2014-09-22 19:43:47 -0400

    absolute function half works
    vondurvel

--- oop.py
+++ oop.py
@@ -20,7 +20,9 @@ def edmas(e):
         if op == '&': #can't use sqrt because it likely gets picked up as a character from a-z
             if '-' not in str(n1): #check if the sqrt is negative which obviously won't work
                 return math.sqrt(n2) # i have _no_ _idea_ if this will work in any way
-
+        if op == 'abs':
+	        return math.fabs(n2)
+			
     ## Solve all of one type of operation
     def solve(op, eq):
         i = 0
@@ -30,6 +32,9 @@ def edmas(e):
                 if op == '&':
                     eq[i] = operator(op, 0, float(eq[i+1]))
                     eq.pop(i+1)
+                elif op == 'abs':
+                    eq[i] = operator(op, float(eq[i-1]), float(eq[i+1]))
+                    eq.pop(i+1)					
                 else:
                     eq[i-1] = operator(op, float(eq[i-1]), float(eq[i+1]))
                     eq.pop(i)
@@ -39,7 +44,7 @@ def edmas(e):
             i += 1
         return eq
 
-    ops = ['^', '&', '/', '*', '-', '+']     # Order of operations
+    ops = ['^', '&', 'abs', '/', '*', '-', '+']     # Order of operations
     e = re.split('([^a-zA-Z0-9._])', e)     # Split equation into parts (numbers and operators)
 
     ## Remove empty spaces
